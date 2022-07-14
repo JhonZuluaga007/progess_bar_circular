bfb796b1b10b8820da941ba2e77ded3463b39982
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2022-07-13 23:08:01 -0500

    changing value dynamically and an animation is applied so that it looks better visually

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -9,16 +10,60 @@ class CircularProgressPage extends StatefulWidget {
   State<CircularProgressPage> createState() => _CircularProgressPageState();
 }
 
-class _CircularProgressPageState extends State<CircularProgressPage> {
+class _CircularProgressPageState extends State<CircularProgressPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late double percentage;
+  late double newPercentage;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 800));
+
+    percentage = 0.0;
+    newPercentage = 0.0;
+
+    animationController.addListener(() {
+      //debugPrint('Valor controller: ${animationController.value}');
+      setState(() {
+        percentage =
+            lerpDouble(percentage, newPercentage, animationController.value)!;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        backgroundColor: Colors.pink,
+        onPressed: () {
+          animationController.forward(from: 0.0);
+          setState(() {
+            percentage = newPercentage;
+            newPercentage += 10;
+            if (newPercentage > 100) {
+              newPercentage = 0;
+              percentage = 0;
+            }
+          });
+        },
+      ),
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(5),
           width: 300,
           height: 300,
-          child: CustomPaint(painter: _MyRadialPainter(percentage: 50)),
+          child: CustomPaint(painter: _MyRadialPainter(percentage: percentage)),
         ),
       ),
     );
